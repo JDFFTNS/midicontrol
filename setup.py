56141134b7c18d5756595aace4c2c842549e4e97
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2017-01-29 20:41:18 +0100

    correct download url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Wolfgang Steiner',
   author_email = 'wolfgang.steiner@gmail.com',
   url = 'https://github.com/wolfgangsteiner/midicontrol', # use the URL to the github repo
-  download_url = 'https://github.com/wolfgangsteiner/midicontrol/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/wolfgangsteiner/midicontrol/tarball/0.1.2', # I'll explain this in a second
   keywords = ['midi', 'control'], # arbitrary keywords
   classifiers = [],
   install_requires=['mido','python-rtmidi'],
